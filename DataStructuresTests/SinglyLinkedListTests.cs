8e988a47090dadcf8d91d8cb71b27e4580986b34
Quwam Ogunlaja <quwam32@gmail.com>
2022-08-01 17:40:41 +0100

    Wrote tests for the AddHead, AddTail, RemoveHead, RemoveTail methods

---
+++ DataStructuresTests/SinglyLinkedListTests.cs
@@ -0,0 +1,108 @@
+using DataStructures;
+
+namespace DataStructuresTests;
+
+[TestClass]
+public class SinglyLinkedListTests
+{
+    [TestMethod]
+    public void InitializeEmptyTest()
+    {
+        SinglyLinkedList<int> ints = new SinglyLinkedList<int>();
+        Assert.AreEqual(0, ints.Count);
+    }
+    
+    [TestMethod]
+    public void AddHeadTest()
+    {
+        var ints = new SinglyLinkedList<int>();
+        for (int i = 1; i <= 5; i++)
+        {
+            ints.AddHead(i);
+            Assert.AreEqual(i, ints.Count);
+            Assert.AreEqual(i, ints.Head!.Value);
+        }
+        
+        int expected = 5;
+        var currentNode = ints.Head;
+
+        while (currentNode != null)
+        {
+            Assert.AreEqual(expected--, currentNode.Value);
+            currentNode = currentNode.Next;
+        }
+    }
+
+    [TestMethod]
+    public void AddTailTest()
+    {
+        var ints = new SinglyLinkedList<int>();
+        for (int i = 1; i <= 5; i++)
+        {
+            ints.AddTail(i);
+            Assert.AreEqual(i, ints.Count);
+            Assert.AreEqual(i, ints.Tail!.Value);
+            Assert.AreEqual(null, ints.Tail.Next);
+        }
+
+        int expected = 1;
+        var currentNode = ints.Head;
+        while (currentNode != null)
+        {
+            Assert.AreEqual(expected++, currentNode.Value);
+            currentNode = currentNode.Next;
+        }
+    }
+
+    [TestMethod]
+    public void RemoveHeadTest()
+    {
+        var ints = Create(5, 1);
+
+        for (int i = 5; i >= 1; i--)
+        {
+            Assert.AreEqual(i, ints.Head!.Value);
+            Assert.AreEqual(i, ints.Count);
+            ints.RemoveHead();
+        }
+
+        Assert.AreEqual(null, ints.Head);
+        Assert.AreEqual(0, ints.Count);
+    }
+
+    [TestMethod]
+    public void RemoveTailTest()
+    {
+        var ints = Create(1, 5);
+
+        for (int i = 5; i >= 1; i--)
+        {
+            Assert.AreEqual(i, ints.Tail!.Value);
+            Assert.AreEqual(i, ints.Count);
+            ints.RemoveTail();
+        }
+
+        Assert.AreEqual(null, ints.Tail);
+        Assert.AreEqual(0, ints.Count);
+    }
+
+    private SinglyLinkedList<int> Create(int start, int end)
+    {
+        var ints = new SinglyLinkedList<int>();
+        if (start < end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                ints.AddTail(i);
+            }
+        } 
+        else
+        {
+            for (int i = start; i >= end; i--)
+            {
+                ints.AddTail(i);
+            }
+        }
+        return ints;
+    }
+}
